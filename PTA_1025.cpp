932d95ced07e1aac4232359517609cbe7fc8179c
Rs1124 <604347974@qq.com>
2022-04-07 21:19:07 +0800

    (段错误:可能是递归深度太深导致堆栈溢出)题目链接: https://pintia.cn/problem-sets/994805260223102976/problems/994805296180871168

--- PTA_1025.cpp
+++ PTA_1025.cpp
@@ -1,42 +1,87 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<iomanip>
 using namespace std;
-#define MAXSIZE 100005
+#define MAXSIZE 1000005
 
-typedef struct{
-	int now; //结点当前地址 
-	int next; //结点的下一个地址 
-	int data; 
-}Point;
-int address,num,flag; //address:首地址、num:结点总个数、flag:需要反转的结点个数
-Point arr[MAXSIZE];
-Point p[MAXSIZE];
+typedef struct Node{
+    int index; //在数组中的索引
+    int now; //结点当前地址
+    int next; //结点的下一个地址
+    int data;
+}Node;
 
-void create(Point *arr){
-	int flag1=num-1;
-	while(flag1!=num){
-		
-	}
-}
+void createList(int num,Node array[]);
+int reverseList(Node array[], int k,int index, int &m, int &temp, int &tempBegin, int &end);
+void display(Node array[], int begin);
 
 int main(){
-	cin>>address>>num>>flag;
-	int num1,num2,num3;
-	for(int i=0;i<num;i++){
-		cin>>num1>>num2>>num3;
-		arr[i].now=num1;
-		arr[i].data=num2;
-		arr[i].next=num3;
-		if(num1==address){
-			p[0].now=num1;
-			p[0].data=num2;
-			p[0].next=num3;
-		}
-	} 
-	cout<<endl;
-	for(int i=0;i<num;i++){
-		printf("%05d ",arr[i].now); //不足5位，在左侧补零
-		printf("%d ",arr[i].data);
-		if(arr[i].next==-1) cout<<-1<<endl;
-		else printf("%05d\n",arr[i].next);
-	} 
+    int begin,num,k,m,temp,tempBegin=-1,end;
+    cin>>begin>>num>>k;
+    Node *array;
+    array = new Node[MAXSIZE]{};
+    temp=k;
+    createList(num, array);
+    if(k>1){
+        m=num%k;
+        reverseList(array,k,begin,m,temp,tempBegin,end);
+    }
+    if(k==1||k==0) tempBegin=begin;
+//    cout<<array[0].now<<" "<<array[0].data<<" "<<array[0].next<<endl; //4
+//    cout<<array[33218].now<<" "<<array[33218].data<<" "<<array[33218].next<<endl; //3
+//    cout<<array[12309].now<<" "<<array[12309].data<<" "<<array[12309].next<<endl; //2
+//    cout<<array[100].now<<" "<<array[100].data<<" "<<array[100].next<<endl; //1
+//    cout<<array[99999].now<<" "<<array[99999].data<<" "<<array[99999].next<<endl; //5
+//    cout<<array[68237].now<<" "<<array[68237].data<<" "<<array[68237].next<<endl; //6
+    display(array,tempBegin);
+}
+
+void createList(int num,Node array[]){
+    for(int i=0;i<num;i++){
+        Node node;
+        int now,next,data;
+        cin>>now>>data>>next;
+        node.now=now;
+        node.data=data;
+        node.next=next;
+        array[now]=node;
+    }
+}
+
+void display(Node array[], int begin){
+    int index=begin;
+    while(index!=-1){
+        cout<<setw(5) << setfill('0')<<array[index].now<<" ";
+        cout<<array[index].data<<" ";
+        if(array[index].next==-1) cout<<-1<<endl;
+        else cout<<setw(5) << setfill('0')<<array[index].next<<endl;
+        index=array[index].next;
+    }
+}
+
+int reverseList(Node array[], int k,int index, int &m, int &temp, int &tempBegin,int &end){
+    if(array[index].next==-1) return index;
+    else{
+        int next = reverseList(array, k, array[index].next,m,temp,tempBegin,end);
+        if(next==-2) return index;
+        if(m>0){
+            if(m==1) tempBegin=next;
+            m--;
+            return index;
+        }
+        if(temp==k){
+            end=array[index].next;
+            temp--;
+        }
+        temp--;
+        array[next].next=index; //逆转
+//        cout<<array[next].data<<" "<<array[next].next<<endl;
+        if(temp==0){
+            array[index].next=tempBegin;
+//            cout<<array[index].data<<" "<<array[index].next<<endl;
+            tempBegin=end;
+            temp=k;
+            return -2;
+        }
+        return index;
+    }
 }
