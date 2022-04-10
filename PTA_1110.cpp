f800d9c5aa0cec1e64f648eb7491bc824fcb3a45
Rs1124 <604347974@qq.com>
2022-04-10 19:59:06 +0800

    题目链接: https://pintia.cn/problem-sets/994805260223102976/problems/1478634682663895040

--- PTA_1110.cpp
+++ PTA_1110.cpp
@@ -1,32 +1,44 @@
 #include<iostream>
 #include<iomanip>
+#include<cmath>
 using namespace std;
 #define MAXSIZE 100005
 
 typedef struct Node{
-    int index; //在数组中的索引
     int now; //结点当前地址
     int next; //结点的下一个地址
     int data;
 }Node;
 
+typedef struct Block{
+    int begin;
+    int end;
+    int next;
+}Block;
+
 void createList(int num,Node array[]);
-int reverseList(Node array[], int index, int &m, int &blockEnd, int &prevEnd, int k, int &listBegin, bool &flag, int mod);
-void display(Node array[], int begin);
+void createBlock(Node array[], int begin, Block arrayBlock[], int &length, int k);
+void display(Node array[], int begin, Block arrayBlock[], int blockNum);
+void reservesBlock(Block arrayBlock[],int blockNum, Node array[]);
 
 int main(){
-    int begin,num,k,m=1,index,listBegin;
-    int blockEnd=0, prevEnd=-999;
-    bool flag=false;
+    int begin,num,k,listBegin,length=0,blockBegin;
     cin>>begin>>num>>k;
-    int mod=num%k;
+    float blockNum= (float)num/(float)k;
+    blockNum= ceil(blockNum);
     Node *array;
     array = new Node[MAXSIZE]{};
-    index=begin;
+    Block *arrayBlock;
+    arrayBlock = new Block[int(blockNum+1)]{};
     createList(num, array);
-    reverseList(array, index, m, blockEnd, prevEnd, k, listBegin, flag, mod);
-    array[blockEnd].next=-1;
-    display(array, listBegin);
+    createBlock(array,begin,arrayBlock,length,k);
+    arrayBlock[int(blockNum)].begin=-1;
+    arrayBlock[int(blockNum)].end=-1;
+    arrayBlock[int(blockNum)].next=-1;
+//    display(array, listBegin,arrayBlock,blockNum);
+    reservesBlock(arrayBlock, blockNum,array);
+    blockBegin=arrayBlock[(int)blockNum-1].begin;
+    display(array,blockBegin,arrayBlock,blockNum);
     return 0;
 }
 
@@ -42,8 +54,28 @@ void createList(int num,Node array[]){
     }
 }
 
-void display(Node array[], int begin){
+void createBlock(Node array[], int begin, Block arrayBlock[], int &length, int k){
+    int index=begin,start,end,next,temp=1;
+    while(index!=-1){
+        if(temp==1) start=index;
+        if(temp==k||(temp<k && array[index].next==-1)){
+            end=index;
+            next=array[index].next;
+            temp=0;
+            Block block;
+            block.begin=start;
+            block.end=end;
+            block.next=next;
+            arrayBlock[length++]=block;
+        }
+        temp++;
+        index=array[index].next;
+    }
+}
+
+void display(Node array[], int begin, Block arrayBlock[], int blockNum){
     int index=begin;
+//    cout<<"链表如下:"<<endl;
     while(index!=-1){
         cout<<setw(5) << setfill('0')<<array[index].now<<" ";
         cout<<array[index].data<<" ";
@@ -51,47 +83,13 @@ void display(Node array[], int begin){
         else cout<<setw(5) << setfill('0')<<array[index].next<<endl;
         index=array[index].next;
     }
+//    cout<<"区块如下:"<<endl;
+//    for(int i=0;i<blockNum+1;i++) cout<<arrayBlock[i].begin<<" "<<arrayBlock[i].end<<" "<<arrayBlock[i].next<<endl;
 }
 
-int reverseList(Node array[], int index, int &m, int &blockEnd, int &prevEnd, int k, int &listBegin, bool &flag, int mod){
-    int temp;
-    if(array[index].next==-1){
-        blockEnd=index;
-        m++;
-        if(mod==1 &&flag==false){
-            listBegin=index;
-            flag=true;
-            m=1;
-        }
-        if(k==1) listBegin=index;
-        return index;
+void reservesBlock(Block arrayBlock[],int blockNum, Node array[]){
+    for(int i=blockNum-1;i>=0;i--){
+        array[arrayBlock[i].end].next=arrayBlock[i-1].begin;
+        if(i==0) array[arrayBlock[i].end].next=-1;
     }
-    else temp = reverseList(array, array[index].next, m, blockEnd, prevEnd, k ,listBegin, flag, mod); //index=5 temp=6
-    if(k==1){
-        blockEnd=index;
-        array[temp].next=index;
-        return index;
-    }
-    if(m==1){
-        if((array[index].next!=-1&&array[temp].next!=-1)||mod==1){
-            prevEnd=blockEnd;
-            blockEnd=index;
-        }
-    }
-    if(mod>1&&flag==false){
-        if(m==mod){
-            listBegin = index;
-            m=0;
-            flag=true;
-        }
-    }
-    else{
-        if(m==k){
-            if(prevEnd!=-999) array[prevEnd].next=index;
-            else listBegin = index;
-            m=0;
-        }
-    }
-    m++;
-    return index;
 }
