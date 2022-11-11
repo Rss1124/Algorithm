fa37d5d44da039a1a3dbda5c0bee59d545e57fb7
Rs1124 <604347974@qq.com>
2022-11-11 17:26:48 +0800

    更改了一下冒泡排序中第二个for循环

--- BubbleSort.c
+++ BubbleSort.c
@@ -2,34 +2,34 @@
 
 void BubbleSort(int a[]); //冒泡排序
 void display(int a[]);
+void swap(int i, int j, int a[]);
 
 int main() {
-    int a1[8] = {5,1,3,4,2,8,6,7}; //不带哨兵的数组
-    int a2[9] = {0,5,1,3,4,2,8,6,7}; //带哨兵的数组
-    BubbleSort(a2);
+    int a[8] = {5,1,3,4,2,8,6,7}; //不带哨兵的数组
+    BubbleSort(a);
     return 0;
 }
 
+void swap(int i, int j, int a[]){
+    int temp = a[j];
+    if(a[i]>a[j]){
+        a[j]=a[i];
+        a[i]=temp;
+    }
+}
+
 void BubbleSort(int a[]){
-    int temp,swap;
-    for(int i=1;i<8;i++){
-        swap=0;
-        for(int j=1;j<8;j++){
-            if(a[j]>a[j+1]){
-                temp=a[j+1];
-                a[j+1]=a[j];
-                a[j]=temp;
-                swap=1;
-            }
+    for(int i=0;i<8;i++){
+        for(int j=7;j>i;j--){
+            swap(j-1, j, a);
         }
         display(a);
         printf("\n");
-        if(swap==0) break;
     }
 }
 
 void display(int a[]){
-    for(int i=0;i<=8;i++){
+    for(int i=0;i<8;i++){
         printf("%d",a[i]);
     }
 }
