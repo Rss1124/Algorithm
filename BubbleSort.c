923d68618454773ad505b99162a2a854feb71960
Rs1124 <604347974@qq.com>
2022-11-11 17:29:23 +0800

    不使用&和bool变量给冒泡排序增加"停止条件"

--- BubbleSort.c
+++ BubbleSort.c
@@ -2,7 +2,7 @@
 
 void BubbleSort(int a[]); //冒泡排序
 void display(int a[]);
-void swap(int i, int j, int a[]);
+int swap(int i, int j, int a[]);
 
 int main() {
     int a[8] = {5,1,3,4,2,8,6,7}; //不带哨兵的数组
@@ -10,19 +10,22 @@ int main() {
     return 0;
 }
 
-void swap(int i, int j, int a[]){
+int swap(int i, int j, int a[]){
     int temp = a[j];
     if(a[i]>a[j]){
         a[j]=a[i];
         a[i]=temp;
-    }
+        return 1;
+    } else return 0;
 }
 
 void BubbleSort(int a[]){
+    int flag=0;
     for(int i=0;i<8;i++){
         for(int j=7;j>i;j--){
-            swap(j-1, j, a);
+            flag = swap(j-1, j, a);
         }
+        if(flag==0) break;
         display(a);
         printf("\n");
     }
